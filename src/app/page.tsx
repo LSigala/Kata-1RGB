9a5d6ddf3a9a6bf0a541f46b5f17fe128555a4d4
LSigala <lsigala@arkusnexus.com>
2024-06-20 09:41:20 -0600

    RGB to HEXADECIMAL code in nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,64 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [first, setFirst] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [third, setThird] = useState(0);
+
+  function SendNumbers(e: any){
+    const data = {
+      first: Number(e.target.Red.value),
+      second: Number(e.target.Green.value),
+      third: Number(e.target.Blue.value)
+    }
+    setFirst(data.first)
+    setSecond(data.second)
+    setThird(data.third)
+
+  }
+  
+  const hex = RGBtoHex([first, second, third]);
+  
+  let numbers = new Array<any>()
+
+  function RGBtoHex(decimals = numbers) {
+    if(first >= 0 && first <= 255 && second >= 0 && second <= 255 && third >= 0 && third <= 255){
+      const RGB = `#${decimals.map((d: any) => d.toString(16).padStart(2, '0')).join('').toUpperCase()}`;
+      alert(RGB);
+    }else{
+      alert("Red, Blue and Green have to be from 0 to 255");
+    }
+  }
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <form onSubmit={SendNumbers}>
+      <div className="container mt-5 text-center">
+        <div className="input-group input-group-sm mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="inputGroup-sizing-sm">Red</span>
+          </div>
+          <input type="number" className="form-control" id="Red" required />
         </div>
+        <div className="input-group input-group-sm mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="inputGroup-sizing-sm">Green</span>
+          </div>
+          <input type="number" className="form-control" id="Green" required />
+        </div>
+        <div className="input-group input-group-sm mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="inputGroup-sizing-sm">Blue</span>
+          </div>
+          <input type="number" className="form-control" id="Blue" required />
+        </div>
+      <button type="submit" className="btn btn-primary">Convert to Hexadecimal</button>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </form>
   );
 }
